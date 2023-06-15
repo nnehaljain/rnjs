cc284fba9df07846b79d06a28c3cf669340fe82e
nehaljain <nehal143t@gmail.com>
2023-06-15 12:25:26 +0530

    first commit

---
+++ demonode.js
@@ -0,0 +1,69 @@
+console.log("hello");
+pin=222
+if(pin==222){
+    /* pin*/ 
+console.log("login");
+}
+else if (pin==123){
+    // password
+    console.log("login");
+}
+else{
+    console.log("Not Login");
+}
+ch=2
+switch (ch)
+{
+     case 1:console.log("add");
+        break;
+    case 2:console.log("sub");
+        break;
+    case 3:console.log("div");
+        break;
+    default:console.log("add");
+        break;
+}
+for ( i = 1; i<= 10; i++) {
+    console.log(i);
+}
+
+
+t=2
+for (i= 1;  i<=10;  i++) {
+    console.log(t,"*",i,"=",i*t);
+    if (i%2==0) {
+        console.log(i);
+    }   
+}
+
+i=1
+while (i<=10) {
+    console.log(i);
+    i++
+}
+
+i=1
+do {
+    console.log(i);
+    i++
+} while(i<=10)
+
+
+num1=12
+num1=[1,2,3,4,5]
+
+str="1"
+str=["user1","user2","user3","user4"]
+str[0]="user1"
+str[1]="user2"
+str[2]="user3"
+str[3]="user4"
+
+num=[2,3,4,5]
+console.log(num);
+for ( i = 0; i<5; i++) {
+    console.log(num[i]);
+    
+}
+
+
---
+++ hw1.js
@@ -0,0 +1,13 @@
+//HOMEWORK 1
+var i=2568
+/* while (i!=0) {
+        j=i%10
+        i=i/10
+        console.log(i);
+    }  */
+   
+    for (i; i!=0; i/10) {
+        i=i%10
+        console.log(i);
+        
+    }
